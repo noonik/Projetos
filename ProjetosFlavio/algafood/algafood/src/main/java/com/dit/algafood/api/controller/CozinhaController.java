7aa993d20b165936453ab59475178d68948986ff
Flavio-BS <flaviosantos_tj@hotmail.com>
2022-02-23 01:43:16 -0300

    Customizando a representação do retorno do elemento com Wrapper e @Jackson xml

--- ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
+++ ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dit.algafood.api.model.CozinhasRepresentationsModel;
 import com.dit.algafood.domain.entities.Cozinha;
 import com.dit.algafood.domain.repository.CozinhaRepository;
 
@@ -19,10 +20,16 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
+	
+	@GetMapping(produces = { MediaType.APPLICATION_XML_VALUE })
+	public CozinhasRepresentationsModel listar1(){
+		return new CozinhasRepresentationsModel(cozinhaRepository.listar());
+	}
+	
 	
 	@GetMapping("/{cozinhaId}")
 	public Cozinha buscar(@PathVariable Long cozinhaId) {
--- ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/domain/entities/Cozinha.java
+++ ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/domain/entities/Cozinha.java
@@ -5,11 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
