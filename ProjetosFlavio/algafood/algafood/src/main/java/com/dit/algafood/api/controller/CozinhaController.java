508e3ee33fabed3de6d573bf7383b87eeb6417f8
Flavio-BS <flaviosantos_tj@hotmail.com>
2022-02-23 23:34:16 -0300

    Modelando e Implementando a inclusão de recursos com POST

--- ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
+++ ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
@@ -8,7 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dit.algafood.api.model.CozinhasRepresentationsModel;
@@ -32,7 +35,6 @@ public class CozinhaController {
 		return new CozinhasRepresentationsModel(cozinhaRepository.listar());
 	}
 	
-	
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);	
@@ -41,5 +43,13 @@ public class CozinhaController {
 		}
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	
 
 }
