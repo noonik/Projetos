a6ea339298e57eaf5d1516ee43c6db426853ace4
Flavio-BS <flaviosantos_tj@hotmail.com>
2022-02-23 23:07:01 -0300

    Utilizando Status Http com @Response

--- ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
+++ ProjetosFlavio/algafood/algafood/src/main/java/com/dit/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.dit.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +34,12 @@ public class CozinhaController {
 	
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);	
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
